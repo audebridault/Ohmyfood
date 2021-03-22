5a0ff169b6d9d7b839a26b340916fcc6d0ee4581
bridault <oumsouleymenboua@gmail.com>
2021-03-22 11:27:53 +0100

    accueil

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,35 @@
-'use strict';
- 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
 var ejs = require("gulp-ejs");
-var cleanCSS = require('gulp-clean-css');
- 
-sass.compiler = require('node-sass');
+var cleanCSS = require("gulp-clean-css");
+var concat = require("gulp-concat");
 
+sass.compiler = require("node-sass");
 
-function makeCss(){
-  return gulp.src('./src/scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./www/css'));
+function makeCss() {
+  return gulp
+    .src(["./src/scss/base.scss", "./src/**/*.scss"])
+    .pipe(concat("style.css"))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("./www/css"));
 }
 
-function makePage(){
-    return gulp.src("./src/pages/*.html")
-    .pipe(ejs({}))
-    .pipe(gulp.dest("./www"));
+function makePage() {
+  return gulp.src("./src/pages/*.html").pipe(ejs({})).pipe(gulp.dest("./www"));
 }
 
-function watch(){
+function watch() {
+  browserSync.init({
+    server: "./www",
+  });
 
-    browserSync.init({
-        server: "./www"
-    });
-
-    gulp.watch("./src/pages/*.html", makePage);
-    gulp.watch("./src/partials/*.html", makePage);
-    gulp.watch("./src/scss/*.scss", makeCss);
-    gulp.watch("./src/partials/**/*.scss", makeCss);
-    gulp.watch("www").on('change', browserSync.reload);
+  gulp.watch("./src/**/*.html", makePage);
+  gulp.watch("./src/**/*.scss", makeCss);
+  gulp.watch("www").on("change", browserSync.reload);
 }
 
 module.exports.makeCss = makeCss;
